b78674b06ee171c9c4b217d8c02e07f54325421a
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-02-18 19:51:37 +0600

    added

---
+++ js/app.js
@@ -0,0 +1,7 @@
+var googleIcon = document.getElementById("googleIcon");
+googleIcon.onclick = function(){
+    document.getElementById('input').style.width = "500px";
+    document.getElementById('input').style.padding = "1px 20px"
+    
+    document.getElementById('micIcon').style.width = "20px"
+}
